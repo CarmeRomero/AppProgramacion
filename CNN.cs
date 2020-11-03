dd1e560682afa85f0f24a5bea5b8d2578e1e92c6
1w1111989CorreaCausaPabloAugusto <69556999+1w1111989CorreaCausaPabloAugusto@users.noreply.github.com>
2020-11-03 09:12:16 -0300

    Cambios
    CAmbios Ehchos en el panel

---
+++ CNN.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FormProgramacion
+{
+    class CNN
+    {
+        
+        SqlConnection cnn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        string cadenaConexion;
+        public CNN()
+        {
+            cnn = new SqlConnection();
+            cmd = new SqlCommand();
+            dr = null;
+            cadenaConexion = @"Data Source=DESKTOP-B8TK374;Initial Catalog=CINE;Integrated Security=True";
+        }
+
+        public CNN(string cadenaConexion)
+        {
+            this.cnn = new SqlConnection();
+            this.cmd = new SqlCommand();
+            this.dr = null;
+            this.cadenaConexion = cadenaConexion;
+        }
+
+        public SqlCommand pCmd { get => cmd; set => cmd = value; }
+        public SqlDataReader pDr { get => dr; set => dr = value; }
+        public string pCadenaConexion { get => cadenaConexion; set => cadenaConexion = value; }
+
+        public void conectar()
+        {
+            cnn.ConnectionString = cadenaConexion;
+            cnn.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+        }
+        public void desconectar()
+        {
+            cnn.Close();
+        }
+        public DataTable extraerTabla(string nombreTabla)
+        {
+            conectar();
+            DataTable dt = new DataTable();
+            cmd.CommandText = "SELECT * FROM " + nombreTabla;
+            dt.Load(cmd.ExecuteReader());
+            desconectar();
+            return dt;
+        }
+
+        public void leerTabla(string nombreTabla)
+        {
+            conectar();
+            cmd.CommandText = "SELECT * FROM " + nombreTabla;
+            dr = cmd.ExecuteReader();
+        }
+        public void actualizarBD(string sentenciaSQL)
+        {
+            this.conectar();
+            this.cmd.CommandText = sentenciaSQL;
+            this.cmd.ExecuteNonQuery();
+            this.desconectar();
+
+        }
+        public void nuevo(string consultaSQL, string p1, int p2, int p3, double p4, DateTime p5)
+        {
+            conectar();
+            cmd.CommandText = consultaSQL;
+            cmd.Parameters.AddWithValue("@param1", p1);
+            cmd.Parameters.AddWithValue("@param2", p2);
+            cmd.Parameters.AddWithValue("@param3", p3);
+            cmd.Parameters.AddWithValue("@param4", p4);
+            cmd.Parameters.AddWithValue("@param5", p5);
+            cmd.ExecuteNonQuery();
+            desconectar();
+            cmd.Parameters.Clear();
+        }
+        public void editar(string consultaSQL, string p1, int p2, int p3, double p4, DateTime p5, int pk)
+        {
+            conectar();
+            cmd.CommandText = consultaSQL;
+            cmd.Parameters.AddWithValue("@param1", p1);
+            cmd.Parameters.AddWithValue("@param2", p2);
+            cmd.Parameters.AddWithValue("@param3", p3);
+            cmd.Parameters.AddWithValue("@param4", p4);
+            cmd.Parameters.AddWithValue("@param5", p5);
+            cmd.Parameters.AddWithValue("@pk", pk);
+            cmd.ExecuteNonQuery();
+            desconectar();
+            cmd.Parameters.Clear();
+        }
+        public void borrar(int codigo)
+        {
+            string sentenciaSQL = "";
+            conectar();
+            sentenciaSQL = "DELETE FROM producto " +
+                    "WHERE codigo = " + codigo;
+            cmd.CommandText = sentenciaSQL;
+            cmd.ExecuteNonQuery();
+            desconectar();
+
+        }
+    }
+}
---
+++ Peliculas.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace FormProgramacion
+{
+    partial class Peliculas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lstPeliculas = new System.Windows.Forms.ListBox();
+            this.cboClasificacion = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboIdiomas = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboGenero = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNacionalidad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.FlatAppearance.BorderSize = 0;
+            this.btnBorrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrar.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnBorrar.Location = new System.Drawing.Point(285, 311);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(99, 48);
+            this.btnBorrar.TabIndex = 17;
+            this.btnBorrar.Text = "Borrar Pelicula";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCancelar.Location = new System.Drawing.Point(108, 396);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(99, 36);
+            this.btnCancelar.TabIndex = 19;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click_1);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.FlatAppearance.BorderSize = 0;
+            this.btnEditar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnEditar.Location = new System.Drawing.Point(167, 311);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(99, 48);
+            this.btnEditar.TabIndex = 20;
+            this.btnEditar.Text = "Editar Pelicula";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click_1);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.FlatAppearance.BorderSize = 0;
+            this.btnNuevo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnNuevo.Location = new System.Drawing.Point(49, 311);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(99, 48);
+            this.btnNuevo.TabIndex = 21;
+            this.btnNuevo.Text = "Nueva Pelicula";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click_1);
+            // 
+            // lstPeliculas
+            // 
+            this.lstPeliculas.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPeliculas.FormattingEnabled = true;
+            this.lstPeliculas.ItemHeight = 21;
+            this.lstPeliculas.Location = new System.Drawing.Point(439, 25);
+            this.lstPeliculas.Name = "lstPeliculas";
+            this.lstPeliculas.Size = new System.Drawing.Size(277, 319);
+            this.lstPeliculas.TabIndex = 16;
+            this.lstPeliculas.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // cboClasificacion
+            // 
+            this.cboClasificacion.FormattingEnabled = true;
+            this.cboClasificacion.Location = new System.Drawing.Point(115, 245);
+            this.cboClasificacion.Name = "cboClasificacion";
+            this.cboClasificacion.Size = new System.Drawing.Size(121, 21);
+            this.cboClasificacion.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label5.Location = new System.Drawing.Point(14, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Clasificacion";
+            // 
+            // cboIdiomas
+            // 
+            this.cboIdiomas.FormattingEnabled = true;
+            this.cboIdiomas.Location = new System.Drawing.Point(115, 190);
+            this.cboIdiomas.Name = "cboIdiomas";
+            this.cboIdiomas.Size = new System.Drawing.Size(121, 21);
+            this.cboIdiomas.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nacionalidad";
+            // 
+            // cboGenero
+            // 
+            this.cboGenero.FormattingEnabled = true;
+            this.cboGenero.Location = new System.Drawing.Point(115, 81);
+            this.cboGenero.Name = "cboGenero";
+            this.cboGenero.Size = new System.Drawing.Size(121, 21);
+            this.cboGenero.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(45, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Genero";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(54, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Titulo";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(115, 25);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(121, 20);
+            this.txtTitulo.TabIndex = 6;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.FlatAppearance.BorderSize = 0;
+            this.btnGrabar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnGrabar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrabar.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrabar.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnGrabar.Location = new System.Drawing.Point(230, 390);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(99, 48);
+            this.btnGrabar.TabIndex = 20;
+            this.btnGrabar.Text = "Guardar Cambios";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(49, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Idioma";
+            // 
+            // txtNacionalidad
+            // 
+            this.txtNacionalidad.Location = new System.Drawing.Point(115, 136);
+            this.txtNacionalidad.Name = "txtNacionalidad";
+            this.txtNacionalidad.Size = new System.Drawing.Size(121, 20);
+            this.txtNacionalidad.TabIndex = 22;
+            // 
+            // Peliculas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNacionalidad);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.lstPeliculas);
+            this.Controls.Add(this.cboClasificacion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cboIdiomas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboGenero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitulo);
+            this.Name = "Peliculas";
+            this.Text = "Peliculas";
+            this.Load += new System.EventHandler(this.Peliculas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.ListBox lstPeliculas;
+        private System.Windows.Forms.ComboBox cboClasificacion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboIdiomas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboGenero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNacionalidad;
+    }
+}
