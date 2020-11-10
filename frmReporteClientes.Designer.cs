c9933e43e7238fbb06f3cee65da89f60b7f6ed7f
1w1111989CorreaCausaPabloAugusto <69556999+1w1111989CorreaCausaPabloAugusto@users.noreply.github.com>
2020-11-10 17:00:08 -0300

    Filtros
    BOTONES Y FILTROS.

--- frmReporteClientes.Designer.cs
+++ frmReporteClientes.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource7 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.btnImprimirTodo = new System.Windows.Forms.Button();
             this.btnImprimirFiltro = new System.Windows.Forms.Button();
@@ -43,51 +45,63 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.CINEDataSet = new FormProgramacion.CINEDataSet();
+            this.TABLA_COMPLETABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.TABLA_COMPLETATableAdapter = new FormProgramacion.CINEDataSetTableAdapters.TABLA_COMPLETATableAdapter();
+            this.chkEdades = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CINEDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TABLA_COMPLETABindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
+            reportDataSource7.Name = "DataSet1";
+            reportDataSource7.Value = this.TABLA_COMPLETABindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource7);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "FormProgramacion.rptClientes.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(28, 21);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(725, 233);
+            this.reportViewer1.Size = new System.Drawing.Size(725, 345);
             this.reportViewer1.TabIndex = 0;
             // 
             // btnImprimirTodo
             // 
-            this.btnImprimirTodo.Location = new System.Drawing.Point(54, 296);
+            this.btnImprimirTodo.Location = new System.Drawing.Point(58, 372);
             this.btnImprimirTodo.Name = "btnImprimirTodo";
             this.btnImprimirTodo.Size = new System.Drawing.Size(75, 37);
             this.btnImprimirTodo.TabIndex = 1;
             this.btnImprimirTodo.Text = "Imprimir Todo";
             this.btnImprimirTodo.UseVisualStyleBackColor = true;
+            this.btnImprimirTodo.Click += new System.EventHandler(this.btnImprimirTodo_Click);
             // 
             // btnImprimirFiltro
             // 
-            this.btnImprimirFiltro.Location = new System.Drawing.Point(639, 305);
+            this.btnImprimirFiltro.Location = new System.Drawing.Point(678, 375);
             this.btnImprimirFiltro.Name = "btnImprimirFiltro";
             this.btnImprimirFiltro.Size = new System.Drawing.Size(75, 37);
             this.btnImprimirFiltro.TabIndex = 2;
             this.btnImprimirFiltro.Text = "Imprimir Filtrado";
             this.btnImprimirFiltro.UseVisualStyleBackColor = true;
+            this.btnImprimirFiltro.Click += new System.EventHandler(this.btnImprimirFiltro_Click);
             // 
             // txtApellido
             // 
-            this.txtApellido.Location = new System.Drawing.Point(454, 306);
+            this.txtApellido.Location = new System.Drawing.Point(458, 382);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(161, 20);
             this.txtApellido.TabIndex = 3;
             // 
             // txtEdad1
             // 
-            this.txtEdad1.Location = new System.Drawing.Point(454, 344);
+            this.txtEdad1.Location = new System.Drawing.Point(458, 420);
             this.txtEdad1.Name = "txtEdad1";
             this.txtEdad1.Size = new System.Drawing.Size(38, 20);
             this.txtEdad1.TabIndex = 4;
             // 
             // txtEdad2
             // 
-            this.txtEdad2.Location = new System.Drawing.Point(521, 344);
+            this.txtEdad2.Location = new System.Drawing.Point(525, 420);
             this.txtEdad2.Name = "txtEdad2";
             this.txtEdad2.Size = new System.Drawing.Size(38, 20);
             this.txtEdad2.TabIndex = 5;
@@ -95,21 +109,21 @@
             // cboTemporada
             // 
             this.cboTemporada.FormattingEnabled = true;
-            this.cboTemporada.Location = new System.Drawing.Point(454, 382);
+            this.cboTemporada.Location = new System.Drawing.Point(458, 458);
             this.cboTemporada.Name = "cboTemporada";
             this.cboTemporada.Size = new System.Drawing.Size(137, 21);
             this.cboTemporada.TabIndex = 6;
             // 
             // dtpInicio
             // 
-            this.dtpInicio.Location = new System.Drawing.Point(454, 421);
+            this.dtpInicio.Location = new System.Drawing.Point(458, 497);
             this.dtpInicio.Name = "dtpInicio";
             this.dtpInicio.Size = new System.Drawing.Size(126, 20);
             this.dtpInicio.TabIndex = 7;
             // 
             // dtpFin
             // 
-            this.dtpFin.Location = new System.Drawing.Point(454, 459);
+            this.dtpFin.Location = new System.Drawing.Point(458, 535);
             this.dtpFin.Name = "dtpFin";
             this.dtpFin.Size = new System.Drawing.Size(126, 20);
             this.dtpFin.TabIndex = 8;
@@ -119,7 +133,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label1.Location = new System.Drawing.Point(370, 307);
+            this.label1.Location = new System.Drawing.Point(374, 383);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 19);
             this.label1.TabIndex = 9;
@@ -130,7 +144,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label2.Location = new System.Drawing.Point(352, 345);
+            this.label2.Location = new System.Drawing.Point(356, 421);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 19);
             this.label2.TabIndex = 10;
@@ -141,7 +155,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label3.Location = new System.Drawing.Point(498, 344);
+            this.label3.Location = new System.Drawing.Point(502, 420);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(17, 19);
             this.label3.TabIndex = 11;
@@ -153,7 +167,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label4.Location = new System.Drawing.Point(350, 382);
+            this.label4.Location = new System.Drawing.Point(354, 458);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(86, 19);
             this.label4.TabIndex = 12;
@@ -164,7 +178,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label5.Location = new System.Drawing.Point(332, 424);
+            this.label5.Location = new System.Drawing.Point(336, 500);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(104, 19);
             this.label5.TabIndex = 13;
@@ -175,18 +189,46 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label6.Location = new System.Drawing.Point(349, 462);
+            this.label6.Location = new System.Drawing.Point(353, 538);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(87, 19);
             this.label6.TabIndex = 14;
             this.label6.Text = "Fin Periodo";
+            // 
+            // CINEDataSet
+            // 
+            this.CINEDataSet.DataSetName = "CINEDataSet";
+            this.CINEDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // TABLA_COMPLETABindingSource
+            // 
+            this.TABLA_COMPLETABindingSource.DataMember = "TABLA_COMPLETA";
+            this.TABLA_COMPLETABindingSource.DataSource = this.CINEDataSet;
+            // 
+            // TABLA_COMPLETATableAdapter
+            // 
+            this.TABLA_COMPLETATableAdapter.ClearBeforeFill = true;
+            // 
+            // chkEdades
+            // 
+            this.chkEdades.AutoSize = true;
+            this.chkEdades.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.chkEdades.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.chkEdades.Location = new System.Drawing.Point(579, 421);
+            this.chkEdades.Name = "chkEdades";
+            this.chkEdades.Size = new System.Drawing.Size(76, 23);
+            this.chkEdades.TabIndex = 16;
+            this.chkEdades.Text = "Edades";
+            this.chkEdades.UseVisualStyleBackColor = true;
+            this.chkEdades.CheckedChanged += new System.EventHandler(this.chkEdades_CheckedChanged);
             // 
             // frmReporteClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
-            this.ClientSize = new System.Drawing.Size(800, 516);
+            this.ClientSize = new System.Drawing.Size(800, 571);
+            this.Controls.Add(this.chkEdades);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -205,6 +247,8 @@
             this.Name = "frmReporteClientes";
             this.Text = "Reporte de Clientes";
             this.Load += new System.EventHandler(this.frmReporteClientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CINEDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TABLA_COMPLETABindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,5 +271,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.BindingSource TABLA_COMPLETABindingSource;
+        private CINEDataSet CINEDataSet;
+        private CINEDataSetTableAdapters.TABLA_COMPLETATableAdapter TABLA_COMPLETATableAdapter;
+        private System.Windows.Forms.CheckBox chkEdades;
     }
 }
--- frmReporteClientes.cs
+++ frmReporteClientes.cs
@@ -12,6 +12,7 @@ namespace FormProgramacion
 {
     public partial class frmReporteClientes : Form
     {
+        CNN cnn = new CNN();
         public frmReporteClientes()
         {
             InitializeComponent();
@@ -19,13 +20,71 @@ namespace FormProgramacion
 
         private void frmReporteClientes_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'CINEDataSet.TABLA_COMPLETA' table. You can move, or remove it, as needed.
+            this.TABLA_COMPLETATableAdapter.Fill(this.CINEDataSet.TABLA_COMPLETA);
 
             this.reportViewer1.RefreshReport();
+            txtEdad1.Enabled = false;
+            txtEdad2.Enabled = false;
+           
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnImprimirTodo_Click(object sender, EventArgs e)
+        {
+            string strSQL = "select Id_cliente, Apellido , Nombre, Tipo_de_compra," +
+                            "Comprobante,Fecha_de_venta,Temporada,Tipo_de_compra,Forma_de_pago,Precio,Sucursal " +
+                            "from TABLA_COMPLETA " +
+                            
+                            "order by 1 ";
+            TABLA_COMPLETABindingSource.DataSource = cnn.consultarTabla(strSQL);
+            reportViewer1.RefreshReport();
+        }
+
+        private void btnImprimirFiltro_Click(object sender, EventArgs e)
+        {
+            string strSQL = "";
+            if (chkEdades.Checked)
+            {
+                
+
+                strSQL = "select Id_cliente, Apellido , Nombre, Tipo_de_compra," +
+                                            "Comprobante,Fecha_de_venta,Temporada,Tipo_de_compra,Forma_de_pago,Precio,Sucursal " +
+                                            "from TABLA_COMPLETA " +
+                                            "where Apellido like '" + txtApellido.Text + "%' AND " +
+                                            "edad between " + txtEdad1.Text + " AND " + txtEdad2.Text +
+                                            " order by 1 ";
+            }
+            else 
+            {
+                strSQL = "select Id_cliente, Apellido , Nombre, Tipo_de_compra," +
+                                            "Comprobante,Fecha_de_venta,Temporada,Tipo_de_compra,Forma_de_pago,Precio,Sucursal " +
+                                            "from TABLA_COMPLETA " +
+                                            "where Apellido like '" + txtApellido.Text + "%'";
+            }
+            
+
+
+            TABLA_COMPLETABindingSource.DataSource = cnn.consultarTabla(strSQL);
+            reportViewer1.RefreshReport();
+        }
+
+        private void chkEdades_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkEdades.Checked == true)
+            {
+                txtEdad1.Enabled = true;
+                txtEdad2.Enabled = true;
+            }
+            if (chkEdades.Checked == false)
+            {
+                txtEdad1.Enabled = false;
+                txtEdad2.Enabled = false;
+            }
         }
     }
 }
